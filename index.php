27586f38559226b898a2db22cab59b4bf7cbf9a8
Isuru Lakmina <58630360+lakmina456@users.noreply.github.com>
2023-08-08 13:08:46 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,42 @@
 <head>
     <title>Welcome To Portfolio!!</title>
     <link rel="stylesheet" href="styles.css">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+       body {
+    margin: 0;
+    padding: 0;
+    background-image: url('images/back3.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+}
+
+.container {
+    text-align: center;
+    padding: 20px;
+    margin: 20px;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
+
+.container form {
+    margin-top: 20px;
+}
+
+/* Add media query for smaller screens */
+@media (max-width: 768px) {
+    .container {
+        padding: 10px;
+    }
+}
+
+    </style>
 </head>
 
 <body style="background-image: url('images/back3.jpg');">
